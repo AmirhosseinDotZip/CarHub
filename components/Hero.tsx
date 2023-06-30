c5b7c262269dbd2eba3f85dff6033c4a3c366f4e
ambient-work <ambio.work@gmail.com>
2023-06-30 14:29:07 +0330

    initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import Image from 'next/image'
+import CustomButton from './CustomButton'
+
+const Hero = () => {
+    const handleScroll = () => { }
+
+    return (
+        <div className='hero'>
+            <div className="flex-1 pt-36 padding-x">
+                <h1 className="hero__title">
+                    Find, Book, or rent a car — quickly and easily!
+                </h1>
+
+                <p className="hero__subtitle">
+                    Streamline your car rental experience with out effortless booking process.
+                </p>
+                <CustomButton
+                    title="Explore Cars"
+                    containerStyles="bg-primary-blue text-white rounded-full mt-10 "
+                    handleClick={handleScroll}
+                />
+            </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image src='/hero.png' alt='hero' fill className='object-contain'></Image>
+                </div>
+                <div className="hero__image-overlay"></div>
+            </div>
+        </div >
+    )
+}
+
+export default Hero
